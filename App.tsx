e4142016171140134169cad2694f9159850b6290
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-03-29 21:41:35 -0300

    🚧 update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 
 import { NativeBaseProvider, VStack, Text } from 'native-base';
+import { THEME } from './src/styles/theme';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <VStack flex={1} bgColor="black">
-        <Text color="white" fontSize={24}>
+    <NativeBaseProvider theme={THEME}>
+      <VStack flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24} fontFamily="heading">
           Hello World!
         </Text>
         <StatusBar style="auto" />
