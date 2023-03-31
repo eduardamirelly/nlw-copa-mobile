b35ab3a0047419aeceb968dec40d5000965347f3
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-03-30 21:13:34 -0300

    🔧 load fonts on app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 
 import { NativeBaseProvider, VStack, Text } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
     <NativeBaseProvider theme={THEME}>
       <VStack flex={1} bgColor="gray.900">
