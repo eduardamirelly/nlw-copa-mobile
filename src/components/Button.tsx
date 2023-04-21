220bcf375a46ce612e7ea4cb57c3a33e6a2a675d
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-20 22:44:21 -0300

    💄 add one more prop to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,20 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
 
 interface ButtonProps extends IButtonProps {
   title: string;
+  type?: 'PRIMARY' | 'SECONDARY';
 }
 
-export function Button({ title, ...rest }: ButtonProps) {
+export function Button({ title, type = 'PRIMARY', ...rest }: ButtonProps) {
   return (
-    <ButtonNativeBase {...rest}>
+    <ButtonNativeBase
+      w="full"
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      textTransform="uppercase"
+      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      {...rest}
+    >
       <Text>
         {title}
       </Text>
