8c984cd94700a158dec640b4725fc6675fb89976
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-23 21:13:24 -0300

    🚧 add interfaces to AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,3 +1,12 @@
 import { createContext } from "react";
 
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+}
+
 export const AuthContext = createContext({});
