0d14887558d2983798ac8fabef12effade025a1d
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-23 21:09:04 -0300

    💄 update SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -7,17 +7,19 @@ import Logo from '../assets/logo.svg';
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button 
+        type="SECONDARY"
         title="ENTRAR COM O GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}        
-        type="SECONDARY"
+        mt={12}
       />
 
-      <Text>
-        Não utilizamos nenhuma informação além do seu e-mail para criação de sua conta.
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   )
