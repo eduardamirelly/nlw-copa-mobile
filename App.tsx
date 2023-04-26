75427792e677816c7c6b6c1adbd3c642cc818e1b
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-25 22:55:07 -0300

    🚧 add AuthContextProvider to App.tsx

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,16 +14,18 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <VStack flex={1} bgColor="gray.900">
-        <StatusBar 
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        {
-          fontsLoaded ? <SignIn /> : <Loading />
-        }
-      </VStack>
+      <AuthContextProvider>
+        <VStack flex={1} bgColor="gray.900">
+          <StatusBar 
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          {
+            fontsLoaded ? <SignIn /> : <Loading />
+          }
+        </VStack>
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
